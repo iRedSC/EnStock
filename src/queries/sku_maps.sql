390a44022119c93cb406d997787cc4b717457405
iRedSC <59147735+iRedSC@users.noreply.github.com>
2025-10-28 19:56:01 -0400

    Remove sqlc stuff

--- src/queries/sku_maps.sql
+++
@@ -1,6 +0,0 @@
--- name: GetSkuMapBySupplier :many
-SELECT spn, sku FROM sku_maps WHERE supplier = :supplier;
-
--- name: InsertSkuMap :exec
-INSERT INTO sku_maps (supplier, spn, sku)
-VALUES (:supplier, :spn, :sku);
--- src/queries/uoms.sql
+++
@@ -1,7 +0,0 @@
--- name: GetUOMAmount :one
-SELECT amount FROM uoms
-WHERE supplier = :supplier AND sku = :sku AND unit = :unit;
-
--- name: InsertUOM :exec
-INSERT INTO uoms (supplier, sku, unit, amount)
-VALUES (:supplier, :sku, :unit, :amount);
